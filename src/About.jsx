3eb3dbcb4926a9f610351184c6b26efc65e58fac
Vivek Singh <vs690799@gmail.com>
2023-04-30 03:24:02 +0530

    updated some files

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <>
             <Nav />
             <div className='about'>
-                <img src="/src/assets/pexels-alfonso-escalante-2832251.jpg" alt="image" />
+                <img src="/src/assets/van.jpg" alt="image" />
                 <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
                 <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
                     (Hitch costs extra 😉)</p>
